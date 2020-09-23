69cd1f22e325748bdd2fe2e0cc40435e9644b7a5
Gabe Jackson <gj@mail.co.de>
2020-09-23 13:12:37 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+On repl.it, select the **Files** tab in the upper-left corner to view the code.
+
+- To install dependencies and set up the virtual environment: `poetry install`
+- To run the code: `poetry run python quickstart/server.py`
+- To run the tests: `poetry run pytest`
---
+++ quickstart/server.py
@@ -0,0 +1,49 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from oso import Oso
+
+from quickstart.expense import db, Expense
+
+oso = Oso()
+oso.register_class(Expense)
+oso.load_file("expenses.polar")
+
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def _respond(self, msg, code=200):
+        self.send_response(code)
+        self.end_headers()
+        self.wfile.write(str(msg).encode() + b"\n")
+
+    def do_GET(self):
+        # 404 if the requested path doesn't match /expenses/:id
+        try:
+            _, resource, id = self.path.split("/")
+            if resource != "expenses":
+                return self._respond("Not Found!", 404)
+        except ValueError:
+            return self._respond("Not Found!", 404)
+
+        # Look up the requested expense in our "database"
+        try:
+            expense = db[int(id)]
+        except KeyError:
+            return self._respond("Not Found!", 404)
+
+        actor = self.headers.get("user", None)
+        action = "GET"
+        if oso.is_allowed(actor, action, expense):
+            self._respond(expense)
+        else:
+            self._respond("Not Authorized!", 403)
+
+
+if __name__ == "__main__":
+    try:
+        from os import environ
+
+        print(
+            f"server running at {environ['REPL_SLUG']}--{environ['REPL_OWNER']}.repl.co"
+        )
+    except KeyError:
+        print("server running at localhost:5050")
+    HTTPServer(("", 5050), RequestHandler).serve_forever()
